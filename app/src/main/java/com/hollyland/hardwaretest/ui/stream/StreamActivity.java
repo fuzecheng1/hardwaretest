92a3354db1779b8eeb1b960f386e093dfeef7b8f
fuzecheng1 <zecheng.fu@hollyland-tech.com>
2021-11-27 11:57:17 +0800

    changeCommit

--- app/src/main/java/com/hollyland/hardwaretest/ui/stream/StreamActivity.java
+++ app/src/main/java/com/hollyland/hardwaretest/ui/stream/StreamActivity.java
@@ -56,6 +56,7 @@ public class StreamActivity extends BaseActivity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        streamTask = new StreamTask();
     }
 
     @SuppressLint("NonConstantResourceId")
